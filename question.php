7ad3048c4e850c0bae412e14b1e5534f726f162d
Hunter Ochabauer <87196096+Bepsi1@users.noreply.github.com>
2023-03-26 15:20:27 -0400

    Add files via upload

--- question.php
+++ question.php
@@ -8,12 +8,15 @@
         $_SESSION['score2'] = $_SESSION['score2'] + 100;
         header("location:index.php");
     }
+    if(isset($_POST['incorrect'])){
+        header("location:index.php");
+    }
 
-    echo("<h1>Question: ".$_SESSION['Question']."</h1><br>");
+    echo("<br><br><h1>Question: ".$_SESSION['Question']."</h1><br>");
 
     $answer = "testing test test";
     if(isset($_POST['showAnswer'])){
-        echo("<h2>What is ".$answer."</h2");
+        echo("<h2>What is ".$answer."</h2><br>");
     }
 
 ?>
@@ -21,11 +24,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<style>
-    h1, h2, #buttons{
-        text-align: center;
-    }
-</style>
+<link rel="stylesheet" href="questionCSS.css">
 </head>
 <body>
 <div id = buttons>
@@ -35,6 +34,7 @@
     <form method="post" action="">
         <input type="submit" name="team1" value="Team 1 +100">
         <input type="submit" name="team2" value="Team 2 +100">
+        <input type="submit" name="incorrect" value="Incorrect Answer">
     </form>
 </body>
 </html>
